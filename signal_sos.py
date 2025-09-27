26fc067772b4501df1643c3e4931bedb44cdee57
mildaa01 <msterba900@gmail.com>
2025-09-27 17:22:22 +0200

    Final update signal_sos.py

--- signal_sos.py
+++ signal_sos.py
@@ -1,12 +1,15 @@
 from gpiozero import Buzzer, LED
 import time
 
+# Set pins to BCM numbering
 led1 = LED(11)
 led2 = LED(12)
 led3 = LED(13)
 buzzer = Buzzer(18)
 
+# First LED that shows ==> S
 def first_led(led1):
+    # Set cycle for 3 repeats
     for i in range(3):
         led1.on()
         buzzer.on()
@@ -15,6 +18,7 @@ def first_led(led1):
         buzzer.off()
         time.sleep(0.3)
 
+# Second LED that shows ==> O
 def second_led(led2):
     for i in range(3):
         led2.on()
@@ -24,6 +28,7 @@ def second_led(led2):
         buzzer.off()
         time.sleep(0.3)
 
+# Third LED that shows ==> S, same as the first LED
 def third_led(led3):
     for i in range(3):
         led3.on()
@@ -33,6 +38,7 @@ def third_led(led3):
         buzzer.off()
         time.sleep(0.3)
 
+# Set the loop cycle
 while True:
     first_led(led1)
     second_led(led2)
